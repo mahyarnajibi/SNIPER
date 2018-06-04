a5240173a1f9174edc732ac92ed3008cd97bc7a4
Mahyar Najibi <mahyarnajibi@gmail.com>
2018-06-04 11:08:59 -0700

    Cleaning up root

--- run.sh
+++
@@ -1,7 +0,0 @@
-python main_chips.py
-cp ~/CRCNN/output/chips_v4_1s/res50_coco_chips/train2014_val2014/CRCNN-0007.params ~/dc2/output/rcnn/cocof/resnet_v1_50_coco_rcnn_dcn_fast/train2014_val2014/rcnn-0004.params
-cd ~/dc2
-python run.py
-cd ~/dc2/output/rcnn/cocof/resnet_v1_50_coco_rcnn_dcn_fast/test-dev2015/results
-zip newsortneg.zip detections_test-dev2015_results.json
-sudo shutdown -h
--- run_res_mx_101_bn.sh
+++
@@ -1,6 +0,0 @@
-python main_chips_res_mx_101.py --cfg configs/faster/res101_mx_bn1.yml
-cp ~/CRCNN/output/chips_resnet101_mx_bn/res101_mx_bn1/train2014_val2014/CRCNNv2-0004.params ~/CRCNN/data/pretrained_model/
-python main_chips_res_mx_101.py --momentum 0.995 --cfg configs/faster/res101_mx_bn2.yml
-cp ~/CRCNN/output/chips_resnet101_mx_bn/res101_mx_bn2/train2014_val2014/CRCNNv2-0002.params ~/CRCNN/data/pretrained_model/
-python main_chips_res_mx_101.py --momentum 0.999 --cfg configs/faster/res101_mx_bn3.yml
-sudo shutdown -h
