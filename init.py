511a2e4019815b223371bd60e1cebf81e4b3439b
Mahyar Najibi <mahyarnajibi@gmail.com>
2019-11-25 13:27:43 -0500

    Moving common imports to init and setting up the logging level

--- init.py
+++ init.py
@@ -1,3 +1,11 @@
 import sys
+import logging
+import os
+import matplotlib
+matplotlib.use('Agg')
+
+os.environ['PYTHONUNBUFFERED'] = '1'
+os.environ['MXNET_CUDNN_AUTOTUNE_DEFAULT'] = '2'
+logging.basicConfig(level=logging.INFO)
 sys.path.insert(0,'lib')
 sys.path.insert(0,'SNIPER-mxnet/python')
